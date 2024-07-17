b384202cea12880d4a7aa74924dd2ad8f896b468
sidiqbimo <sani21bimo@gmail.com>
2024-07-17 12:26:53 +0700

    add new variable

--- app/src/main/java/com/synrgyseveneight/bcarevamp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/synrgyseveneight/bcarevamp/ui/home/HomeFragment.kt
@@ -21,6 +21,7 @@ class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var param3: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
