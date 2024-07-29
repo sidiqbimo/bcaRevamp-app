c4624c7de4794f3d35363a087d726296669609e3
fauziferdi <fauziferdiansyahindo@gmail.com>
2024-07-29 14:08:40 +0700

    Implemented login UI

--- app/src/main/java/com/synrgyseveneight/bcarevamp/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/synrgyseveneight/bcarevamp/ui/auth/LoginFragment.kt
@@ -35,7 +35,7 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return inflater.inflate(R.layout.fragment_login_first, container, false)
     }
 
     companion object {
